27adcb8bf0bf5bbabd79477fc088a5d85963796b
wanruuu <1155107790@link.cuhk.edu.hk>
2024-02-03 18:11:09 +0800

    修复BUG: 新建清单没有传入type

--- app/src/components/common/MyFloatButton.js
+++ app/src/components/common/MyFloatButton.js
@@ -15,6 +15,15 @@ export default function MyFloatButton(props) {
     const [editInvoice, setEditInvoice] = useState(undefined)
     const [messageApi, contextHolder] = message.useMessage()
 
+    const isSales = ['salesOrder', 'salesRefund'].includes(props.type)
+    const isOrder = ['salesOrder', 'purchaseOrder'].includes(props.type)
+    const modalTitle = {
+        'salesOrder': '新建销售清单',
+        'salesRefund': '新建销售退货',
+        'purchaseOrder': '新建采购清单',
+        'purchaseRefund': '新建采购退货'
+    }[props.type] || '错误'
+
     // draft
     const saveDraft = (draft) => {
         var newDrafts = props.drafts
@@ -34,16 +43,19 @@ export default function MyFloatButton(props) {
 
     const columns = [
         { title: '保存时间', dataIndex: 'draftTime', align: 'center', render: time => time.format('HH:mm:ss') },
-        { title: '客户', dataIndex: 'partner', align: 'center' },
-        { title: '产品数', dataIndex: 'items', align: 'center', render: items => {
-            if (props.type === 'salesOrder' || props.type === 'purchaseOrder') return items.length - 1
-            return items.length
-        } },
-        { title: '操作', align: 'center', render: (_, draft) => 
-            <Space.Compact size='small'>
-                <Button type='link' size='small' onClick={_ => setEditInvoice(draft)}>编辑</Button>
-                <Button type='link' danger size='small' onClick={_ => removeDraft(draft)}>删除</Button>
-            </Space.Compact>
+        { title: isSales ? '客户' : '供应商', dataIndex: 'partner', align: 'center' },
+        {
+            title: '产品数', dataIndex: 'items', align: 'center', render: items => {
+                if (isOrder) return items.length - 1
+                return items.length
+            }
+        },
+        {
+            title: '操作', align: 'center', render: (_, draft) =>
+                <Space.Compact size='small'>
+                    <Button type='link' size='small' onClick={_ => setEditInvoice(draft)}>编辑</Button>
+                    <Button type='link' danger size='small' onClick={_ => removeDraft(draft)}>删除</Button>
+                </Space.Compact>
         }
     ]
 
@@ -59,20 +71,20 @@ export default function MyFloatButton(props) {
     return <>
         {contextHolder}
         <Popover title={`草稿箱 (${props.drafts.length})`} placement='topLeft' zIndex={999} trigger='click' content={
-            <Table className='draftTable' dataSource={props.drafts} size='small' pagination={{pageSize: 5, size: 'small'}} 
+            <Table className='draftTable' dataSource={props.drafts} size='small' pagination={{ pageSize: 5, size: 'small' }}
                 hideOnSinglePage bordered columns={columns} />
         } destroyTooltipOnHide>
             <FloatButton icon={<InboxOutlined />} style={{ right: 80 }} badge={{ count: props.drafts.length, color: 'blue' }} />
         </Popover>
-        
-        <FloatButton icon={<PlusOutlined />} type='primary' style={{ right: 24 }} onClick={_ => 
-            setEditInvoice(emptyInvoice(props.type === 'salesOrder' || props.type === 'purchaseOrder' ? 1 : 0))
-        } />
-        
-        <Modal title={getTitle(props.type)} open={editInvoice} width={1000} centered onCancel={_ => setEditInvoice(undefined)} footer={null}>
-            <InvoiceEditView invoice={editInvoice} messageApi={messageApi} 
-            dismiss={_ => setEditInvoice(undefined)} refresh={props.refresh} 
-            saveDraft={saveDraft} removeDraft={removeDraft} />
+
+        <FloatButton icon={<PlusOutlined />} type='primary' style={{ right: 24 }} onClick={_ => {
+            setEditInvoice(emptyInvoice(isOrder ? 1 : 0))
+        }} />
+
+        <Modal title={modalTitle} open={editInvoice} width={1000} centered onCancel={_ => setEditInvoice(undefined)} footer={null}>
+            <InvoiceEditView invoice={editInvoice} messageApi={messageApi} type={props.type}
+                dismiss={_ => setEditInvoice(undefined)} refresh={props.refresh}
+                saveDraft={saveDraft} removeDraft={removeDraft} />
         </Modal>
     </>
 }
