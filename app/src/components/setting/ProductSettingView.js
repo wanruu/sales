91a709fd5a6e885d41372504cba329a1f8242de8
wanruuu <1155107790@link.cuhk.edu.hk>
2024-02-11 03:38:13 +0800

    修复设置页面-单位编辑表格，float right丑的问题

--- app/src/components/setting/ProductSettingView.js
+++ app/src/components/setting/ProductSettingView.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Form, Table, Button, Divider, Card } from 'antd'
+import { Table, Button, Divider, Card } from 'antd'
 
 
 import { DndContext, PointerSensor, useSensor, useSensors } from '@dnd-kit/core'
@@ -17,8 +17,6 @@ import { invoiceSettings } from '../../utils/config'
 import SettingSwitchItem from './SettingSwitchItem'
 import Title from 'antd/es/typography/Title'
 
-const { Item } = Form
-
 
 function UnitSettingView() {
     const [unitOptions, setUnitOptions] = useState(JSON.parse(invoiceSettings.get('unitOptions')))
@@ -26,29 +24,31 @@ function UnitSettingView() {
 
     const editTableColumns = [
         { title: '单位', dataIndex: 'label', width: '100px', align: 'center' },
-        { title: '状态', dataIndex: 'default', width: '150px', align: 'center', render: (_, curUnit) => 
-            curUnit.default ? <b>当前默认单位</b> :
-            <Button size='small' onClick={_ => {
-                const newUnitData = JSON.parse(JSON.stringify(unitOptions))
-                for (const unit of newUnitData) {
-                    unit.default = unit.label === curUnit.label
-                }
-                setUnitOptions(newUnitData)
-            }}>设为默认</Button>
+        {
+            title: '状态', dataIndex: 'default', width: '150px', align: 'center', render: (_, curUnit) =>
+                curUnit.default ? <b>当前默认单位</b> :
+                    <Button size='small' onClick={_ => {
+                        const newUnitData = JSON.parse(JSON.stringify(unitOptions))
+                        for (const unit of newUnitData) {
+                            unit.default = unit.label === curUnit.label
+                        }
+                        setUnitOptions(newUnitData)
+                    }}>设为默认</Button>
         }
     ]
     const viewTableColumns = [
         { title: '序号', width: '32px', align: 'center', render: (_, __, idx) => idx + 1 },
         { title: '单位', dataIndex: 'label', width: '100px', align: 'center' },
-        { title: '状态', dataIndex: 'default', width: '150px', align: 'center', render: (_, curUnit) => 
-            curUnit.default ? <b>当前默认单位</b> :
-            <Button size='small' onClick={_ => {
-                const newUnitData = JSON.parse(JSON.stringify(unitOptions))
-                for (const unit of newUnitData) {
-                    unit.default = unit.label === curUnit.label
-                }
-                setUnitOptions(newUnitData)
-            }}>设为默认</Button>
+        {
+            title: '状态', dataIndex: 'default', width: '150px', align: 'center', render: (_, curUnit) =>
+                curUnit.default ? <b>当前默认单位</b> :
+                    <Button size='small' onClick={_ => {
+                        const newUnitData = JSON.parse(JSON.stringify(unitOptions))
+                        for (const unit of newUnitData) {
+                            unit.default = unit.label === curUnit.label
+                        }
+                        setUnitOptions(newUnitData)
+                    }}>设为默认</Button>
         }
     ]
     const sensors = useSensors(useSensor(PointerSensor, { activationConstraint: { distance: 1, } }))
@@ -78,7 +78,8 @@ function UnitSettingView() {
         const { attributes, listeners, setNodeRef,
             transform, transition, isDragging
         } = useSortable({ id: props['data-row-key'] })
-        const style = { ...props.style,
+        const style = {
+            ...props.style,
             transform: CSS.Transform.toString(transform && { ...transform, scaleY: 1 }),
             transition,
             cursor: 'move',
@@ -93,22 +94,24 @@ function UnitSettingView() {
 
     if (isEditing) {
         return (
-            <Item extra='（1）勾选的单位将会显示在开单页面、产品编辑页面的单位选择列表中，不勾选则不显示。
-                （2）拖动列表项目可以为单位排序。
-                （3）无需手动保存。'>
+            <>
                 <DndContext sensors={sensors} modifiers={[restrictToVerticalAxis]} onDragEnd={onDragEnd}>
                     <SortableContext items={unitOptions.map(i => i.key)} strategy={verticalListSortingStrategy}>
                         <Table size='small' rowKey='key' columns={editTableColumns} dataSource={unitOptions}
-                            pagination={false} components={{ body: { row: TableRow } }} 
+                            pagination={false} components={{ body: { row: TableRow } }}
                             rowSelection={rowSelection}
                             footer={_ =>
                                 <Button size='small' onClick={_ => setIsEditing(false)}>收起</Button>
                             }
-                            
                         />
                     </SortableContext>
                 </DndContext>
-            </Item>
+                <ol style={{ color: 'gray' }}>
+                    <li>勾选的单位将会显示在“开单页面”、“产品编辑页面”的单位选择列表中，不勾选则不显示。</li>
+                    <li>拖动列表项目可以为单位排序。</li>
+                    <li>无需手动保存。</li>
+                </ol>
+            </>
         )
     }
 
@@ -129,9 +132,8 @@ export default function ProductSettingView() {
 
         <Card>
             <Title id='product-material' level={3}>材质</Title>
-            <SettingSwitchItem keyy='ifShowMaterial' value={ifShowMaterial} setValue={setIfShowMaterial} 
+            <SettingSwitchItem keyy='ifShowMaterial' value={ifShowMaterial} setValue={setIfShowMaterial}
                 label='开启材质' help='该开关不会影响原有数据，只是显示或隐藏材质项。' />
-        
             <Divider />
 
             <Title id='product-unit' level={3}>单位</Title>
