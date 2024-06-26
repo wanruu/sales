af064f0f4c202d75fa2f3b471af18ad316fd99df
wanruuu <1155107790@link.cuhk.edu.hk>
2024-06-26 21:07:01 +0800

    优化客户和产品输入的关键词提示

--- app/public/server/routes/promptRoute.js
+++ app/public/server/routes/promptRoute.js
@@ -1,50 +1,62 @@
 const express = require('express')
 const router = express.Router()
+const { pinyin } = require('pinyin-pro')
+
 
 const db = require('../db')
 
 
+const matchSort = (array, keyword) => {
+    return array.filter(name => 
+        name.includes(keyword) ||
+        pinyin(name, { pattern: 'first', toneType: 'none', type: 'array' }).join('').includes(keyword) ||
+        pinyin(name, { toneType: 'none', type: 'array' }).join('').includes(keyword)
+    ).sort((a, b) => a.length - b.length)
+}
+
+
 router.get('/partner/name/:name', (req, res) => {
-    db.all(`SELECT name FROM partner WHERE name LIKE "%${req.params.name}%"`, (err, partners) => {
+    db.all(`SELECT name FROM partner`, (err, partners) => {
         if (err) {
             console.error(err)
             res.status(500).end()
             return
         }
-        res.send(partners.map(partner => partner.name))
+        res.send(matchSort(partners.map(partner => partner.name), req.params.name))
     })
 })
 
+
 router.get('/product/material/:material', (req, res) => {
-    db.all(`SELECT DISTINCT material FROM product WHERE material LIKE "%${req.params.material}%"`, (err, products) => {
+    db.all(`SELECT DISTINCT material FROM product`, (err, products) => {
         if (err) {
             console.error(err)
             res.status(500).end()
             return
         }
-        res.send(products.map(product => product.material))
+        res.send(matchSort(products.map(product => product.material), req.params.material))
     })
 })
 
 router.get('/product/name/:name', (req, res) => {
-    db.all(`SELECT DISTINCT name FROM product WHERE name LIKE "%${req.params.name}%"`, (err, products) => {
+    db.all(`SELECT DISTINCT name FROM product`, (err, products) => {
         if (err) {
             console.error(err)
             res.status(500).end()
             return
         }
-        res.send(products.map(product => product.name))
+        res.send(matchSort(products.map(product => product.name), req.params.name))
     })
 })
 
 router.get('/product/spec/:spec', (req, res) => {
-    db.all(`SELECT DISTINCT spec FROM product WHERE spec LIKE "%${req.params.spec}%"`, (err, products) => {
+    db.all(`SELECT DISTINCT spec FROM product`, (err, products) => {
         if (err) {
             console.error(err)
             res.status(500).end()
             return
         }
-        res.send(products.map(product => product.spec))
+        res.send(matchSort(products.map(product => product.spec), req.params.spec))
     })
 })
 
